7eabd42d1be7fe22603223987e9a858357740f57
samratpodder <iamsamrat16@outlook.com>
2021-04-26 12:14:40 +0530

    added eqsll.c

--- 3rd Sem C/eqSLL.c
+++ 3rd Sem C/eqSLL.c
@@ -38,6 +38,11 @@ void add(Node** list,int data,int exp) {
 void display(Node* list){
 	Node* temp = list;
 	while (temp != NULL) {
+		if (temp->data=0)
+		{
+			continue;
+		}
+		
 		printf("%dx^%d",temp->data,temp->exp);
 		temp = temp->next;
         if(temp!=NULL){
@@ -50,12 +55,41 @@ void display(Node* list){
         
 	}
 }
+Node* sum(Node** list1,Node** list2,Node* res)
+{
+	
+	Node* temp1 = *list1;
+	Node* temp2 = *list2;
+	while (temp1->next!=NULL)
+	{
+		Node* newnode=malloc(sizeof(Node));
+		newnode->data=(temp1->data)+(temp2->data);
+    	newnode->exp=temp1->exp;
+		newnode->next=NULL;
+		if (res==NULL)
+		{
+			res=newnode;
+			continue;
+		}
+		while(res->next!=NULL)
+		{
+			res=res->next;
+		}
+		res->next=newnode;
+		
+	}
+	return res;
+	
+}
 int main() {
 	int input=999;
-	Node* list = NULL;
+	Node* list1 = NULL;
+	Node* list2 = NULL;
+	Node* result=NULL;
 	while(input!=0){
-		printf("Enter 1 to add\n");
+		printf("Enter 1 to create\n");
 		printf("Enter 2 to display\n");
+		printf("Enter 3 to add two lists\n");
 		printf("Enter 0 to exit.\n");
 		scanf("%d",&input);
 		switch (input)
@@ -63,14 +97,35 @@ int main() {
 		case 0:
 			break;
 		case 1:
-			printf("Enter data and exponent:");
+			printf("List 1:\n");
+			printf("Enter highest order:");
 			int data,exp;
-			scanf("%d %d",&data,&exp);
-			add(&list,data,exp);
+			scanf("%d",&exp);
+			for (int i = exp; i >=0  ; i--)
+			{
+				printf("Coefficient of x^%d: ",i);
+				scanf("%d",&data);
+				add(&list1,data,exp);
+			}
+			printf("List 2:\n");
+			printf("Enter highest order:");
+			int data2,exp2;
+			scanf("%d",&exp2);
+			for (int i = exp2; i >=0  ; i--)
+			{
+				printf("Coefficient of x^%d: ",i);
+				scanf("%d",&data2);
+				add(&list2,data,exp);
+			}
 			break;
 		case 2:
-			display(list);
+			display(list1);
+			display(list2);
+			display(result);
 			break;
+		case 3:
+			result=sum(&list1,&list2,result);
+			display(result);
 		default:
 			printf("Not a valid choice.");
 			break;
